53932b7317825ced3051f1e23ce78414239ac154
Levy <golvwei@gmail.com>
2023-12-21 22:46:10 +0800

    OOP-001: Inheritance (#31)

---
+++ Stage-01-Go_Basic/1-5-Object-Oriented_Programming/01-继承.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	id   int
+	name string
+	age  int
+}
+
+type Student struct {
+	Person // 匿名字段，只有类型，没有成员的名字
+	score  float64
+}
+
+type Teacher struct {
+	Person // 匿名字段，只有类型，没有成员的名字
+	salary float64
+}
+
+func main() {
+	// 全部初始化
+	var student = Student{Person{id: 1, name: "Levy", age: 23}, 100}
+	fmt.Println("父类成员和子类成员全部初始化的结果为：", student) // {{1 Levy 23} 100}
+
+	// 部分初始化
+	var teacher = Teacher{Person{id: 2}, 5000}
+	fmt.Println("父类成员部分初始化和子类成员全部初始化的结果为：", teacher) //  {{2  0} 5000}
+}
